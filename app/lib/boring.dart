b7b2e8d4007f67ea068f61021bc319af8b0d1a32
John Clark <socrates37@gmail.com>
2022-08-08 06:44:23 -0600

    Home Page without project

---
+++ app/lib/boring.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+
+class BoringH1 extends StatelessWidget {
+  const BoringH1(
+      {Key? key, required this.text, this.textAlign = TextAlign.start})
+      : super(key: key);
+
+  final String text;
+  final TextAlign textAlign;
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(
+      text,
+      style: const TextStyle(fontSize: 24),
+      textAlign: TextAlign.center,
+    );
+  }
+}
+
+enum BoringButtonRole {
+  primary,
+  secondary,
+  success,
+  link,
+}
+
+class BoringButton extends StatelessWidget {
+  const BoringButton(
+      {Key? key, this.text = '', this.role = BoringButtonRole.primary})
+      : super(key: key);
+
+  final String text;
+  final BoringButtonRole role;
+
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+      style: ElevatedButton.styleFrom(
+        textStyle: const TextStyle(fontSize: 16),
+        padding: const EdgeInsets.all(16.0),
+      ),
+      onPressed: () => {},
+      child: Text(text),
+    );
+  }
+}
